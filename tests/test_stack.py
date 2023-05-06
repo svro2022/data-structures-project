d88e1b3d39ca5373d4dc90bae6935274e612497f
svro <ichistrawberry2012@gmail.com>
2023-05-06 09:57:00 +0300

    changed test_stack.py

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,24 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack
+
+
+class TestStack(unittest.TestCase):
+    """
+    Класс для тестирования класса Stack
+    """
+    def test_init(self):
+        """
+        Тест для инита
+        """
+        stack = Stack()
+        self.assertIsNone(stack.top)
+
+    def test_push(self):
+        """
+        Тест для добавления элемента
+        """
+        stack = Stack()
+        self.assertIsNone(stack.top)
+        stack.push("test")
+        self.assertEqual(stack.top.data, "test")
