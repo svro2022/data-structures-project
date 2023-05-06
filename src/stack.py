34d4c44a0503e1eb093382f82257766ad33bf3c4
svro <ichistrawberry2012@gmail.com>
2023-05-06 09:41:17 +0300

    changed stack.py

--- src/stack.py
+++ src/stack.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,8 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.data = []
+        self.top = None
 
     def push(self, data):
         """
@@ -23,7 +25,9 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        new_node = Node(data, None)
+        new_node.next_node = self.top
+        self.top = new_node
 
     def pop(self):
         """
