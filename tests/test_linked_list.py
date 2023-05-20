e45238c2ec21fefc91607b07ef59954aeca72b5b
svro <ichistrawberry2012@gmail.com>
2023-05-20 21:17:56 +0300

    changed test_linked_list.py for h5

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+
+from src.linked_list import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+    """
+    Класс для тестирования LinkedList
+    """
+
+    def test_linked_list_beginning_at_end(self):
+        ll = LinkedList()
+
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+
+        print(str(ll))
+        assert str(ll) == " {'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
+
